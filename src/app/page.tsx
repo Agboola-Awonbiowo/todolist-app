6cf5e15195ccbe524cb09956230fe88abcf63e6c
agboola <agboolanathaniel@yahoo.com>
2024-12-25 15:08:02 +0100

    remove div from main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
-"use client"
+"use client";
 import Todo from "./components/todo";
 
 export default function Home() {
-  return (
-    <div>
-      <Todo />
-    </div>
-  );
+  return <Todo />;
 }
